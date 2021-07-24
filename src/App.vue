9cb5ced56259de767470f32b04b750d3266349d1
haijinsha <dengshenkk@gmail.com>
2021-07-24 18:38:12 +0800

    feat: 完成点击事件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app" style="width:100%; height:100%">
+    {{ loading }}
+    <div class="camera">
+      <div class="x">{{ cameraPosition.x }}</div>
+      <div class="y">{{ cameraPosition.y }}</div>
+      <div class="z">{{ cameraPosition.z }}</div>
+    </div>
     <div style="position: absolute">
-      <input type="text" v-model="x">
-      <input type="text" v-model="y">
-      <input type="text" v-model="z">
-      <button ref="btn">按钮</button>
+      <input v-model="x" type="text">
+      <input v-model="y" type="text">
+      <input v-model="z" type="text">
+      <button @click="handleBtnClick">按钮</button>
     </div>
     <div class="this.camera" style="position: absolute; right: 0">
       <span v-for="(item,index) of this.cameras" :key="index" @click="handleCamera(item)">{{ item.name }}</span>
@@ -22,29 +28,147 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 // import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js'
+window.THREE = THREE
 export default {
   name: 'App',
   components: {
   },
   data () {
     return {
-      loading: '加载中...',
-      x: -771,
-      y: 854,
-      z: -891,
+      loading: '模型加载中...',
+      x: 4000,
+      y: 8000,
+      z: -18000,
       renderer: null,
       controls: null,
       camera: null,
       cameras: [],
-      url: '/model/3.gltf'
+      url: '/model/3.gltf',
+      cameraPosition: {
+        x: '',
+        y: '',
+        z: ''
+      }
     }
   },
   mounted () {
     // this.init()
-    this.init2()
+    // this.init2()
     // this.handleClick()
+    this.init7211()
   },
   methods: {
+    start ({ scene, camera, renderer, controls }) {
+      const animate = () => {
+        controls.update()
+        requestAnimationFrame(animate)
+        renderer.render(scene, camera)
+        this.cameraPosition.x = parseInt(camera.position.x)
+        this.cameraPosition.y = parseInt(camera.position.y)
+        this.cameraPosition.z = parseInt(camera.position.z)
+      }
+      requestAnimationFrame(animate)
+    },
+    init7211 () {
+      const _this = this
+      const scene = new THREE.Scene()
+      const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 60, 100000)
+      camera.position.set(4000, 8000, -18000)
+      const materialMap = new Map([
+        ['立方体10', { color: new THREE.Color(0X35F5FF), emissive: new THREE.Color(0X35F5FF), opacity: 1 }],
+        ['立方体11', { color: new THREE.Color(0XEE0AB8), emissive: new THREE.Color(0XEE0AB8), opacity: 1 }],
+        ['立方体12', { color: new THREE.Color(0XFFFF00), emissive: new THREE.Color(0XFFFF00), opacity: 1 }],
+        ['立方体13', { color: new THREE.Color(0X35F5FF), emissive: new THREE.Color(0X35F5FF), opacity: 1 }]
+      ])
+      new GLTFLoader().load('/model/7211.gltf', gltf => {
+        scene.add(gltf.scene)
+        console.log('gltf: ', gltf)
+        _this.loading = ''
+        gltf.scene.traverse(function (child) {
+          if (!child.isMesh) return
+          const find = materialMap.get(child.name)
+          if (find) {
+            const material = child.material
+            material.opacity = find.opacity
+            material.color = find.color
+            material.emissive = find.emissive
+          }
+        })
+      })
+
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+      })
+      const controls = new OrbitControls(camera, renderer.domElement)
+      controls.enableRotate = true
+      controls.autoRotate = true
+      controls.autoRotateSpeed = 1
+      // 设置相机距离原点的最远距离
+      controls.minDistance = 5000
+      // 设置相机距离原点的最远距离
+      controls.maxDistance = 20000
+
+      function createLight (x, y, z, color) {
+        const pointLight = new THREE.PointLight(color, 1)
+        pointLight.position.set(x, y, z)
+        return pointLight
+      }
+
+      const light1 = createLight(19000, 11000, -19000)
+      const light2 = createLight(18000, 21000, 19000)
+      const light3 = createLight(-19000, 11000, -19000)
+      const light4 = createLight(-19000, 21000, 19000)
+
+      scene.add(light1)
+      scene.add(light2)
+      scene.add(light3)
+      scene.add(light4)
+      renderer.setClearAlpha(0.2)
+      renderer.setSize(window.innerWidth, window.innerHeight)
+      renderer.setClearColor(0xFFFFFF)
+      document.querySelector('#container').appendChild(renderer.domElement)
+      this.setBackground({ scene })
+      // this.helper({ scene, light1 })
+      this.start({ scene, camera, controls, renderer })
+      this.setMove({ camera })
+      this.handleClick({ renderer, scene, camera })
+    },
+    helper ({ scene, light }) {
+      const helper = new THREE.PointLightHelper(light, 5)
+      scene.add(helper)
+    },
+    setMove ({ camera }) {
+      console.log('camera: ', camera)
+      document.addEventListener('keydown', ev => {
+        switch (ev.key) {
+          case 'ArrowUp':
+          case 'w':
+            console.log('向前')
+            camera.position.z += 200
+            break
+
+          case 'ArrowDown':
+          case 's':
+            console.log('向后')
+            camera.position.z -= 200
+            break
+
+          case 'ArrowLeft':
+          case 'a':
+            console.log('向左')
+            camera.position.x += 200
+            break
+
+          case 'ArrowRight':
+          case 'd':
+            console.log('向右')
+            camera.position.x -= 200
+            break
+        }
+        camera.updateProjectionMatrix()
+      })
+    },
     handleCamera (item) {
       console.log('this.scene: ', this.scene)
       console.log('item: ', item)
@@ -55,49 +179,84 @@ export default {
       } = item.position
       this.camera.position.set(x, y, z)
     },
-    handleClick () {
-      var mouse = new THREE.Vector2()
-
-      window.addEventListener('mousemove', e => {
-        mouse.x = (e.clientX / window.innerWidth) * 2 - 1
-        mouse.y = -(e.clientY / window.innerHeight) * 2 + 1
-        var raycaster = new THREE.Raycaster()
-        raycaster.setFromCamera(mouse, this.camera)
-        var intersects = raycaster.intersectObjects(this.scene.children)
-
-        if (intersects.length) {
-          console.log('intersects: ', intersects)
-        }
+    handleBtnClick () {
+      const { x, y, z } = this
+      this.camera.position.set(x, y, z)
+      this.camera.updateProjectionMatrix()
+    },
+    handleClick ({ renderer, scene, camera }) {
+      renderer.domElement.addEventListener('click', e => {
+        const intersects = getIntersects(e)[0]
+        console.log('intersects: ', intersects.object)
+        // const { y } = intersects.object.position
+        // camera.position.y = y
+        // setTimeout(() => {
+        //   alert(`点击了${intersects.object.name}`)
+        // }, 1000)
       })
-      // if (intersects.length > 0) {
-      //   if (INTERSECTED != intersects[0].object) {
-      //     material.setValues({ color: 'purple' })
-      //     if (INTERSECTED) { material.setValues({ color: 'yellow' }) };
-      //     INTERSECTED = intersects[0].object
-      //   }
-      // } else {
-      //   if (INTERSECTED) { material.setValues({ color: 'yellow' }) };
-      //   INTERSECTED = null
-      // }
+
+      function getIntersects (event) {
+        event.preventDefault()
+        const container = document.querySelector('#container')
+
+        const x =
+          ((event.clientX - container.getBoundingClientRect().left) /
+            container.offsetWidth) *
+          2 -
+          1 // 标准设备横坐标
+
+        // 这里的mainCanvas是个dom元素,getBoundingClientRectangle会返回当前元素的视口大小.
+
+        const y =
+          -(
+            (event.clientY - container.getBoundingClientRect().top) /
+            container.offsetHeight
+          ) *
+          2 +
+          1 // 标准设备纵坐标
+
+        const standardVector = new THREE.Vector3(x, y, 1) // 标准设备坐标
+
+        // 标准设备坐标转世界坐标
+
+        const worldVector = standardVector.unproject(camera)
+
+        // 射线投射方向单位向量(worldVector坐标减相机位置坐标)
+
+        const ray = worldVector.sub(camera.position).normalize()
+
+        // 创建射线投射器对象
+
+        const raycaster = new THREE.Raycaster(camera.position, ray)
+
+        // 获取raycaster直线和所有模型相交的数组集合
+        var intersects = raycaster.intersectObjects(scene.children, true)
+        if (intersects.length > 0) {
+          console.log('点击了物体')
+        }
+
+        // 返回选中的对象
+        return intersects
+      }
     },
     setHelp (light) {
       const helper = new THREE.DirectionalLightHelper(light, 5)
       this.scene.add(helper)
     },
-    setBackground () {
+    setBackground ({ scene }) {
       // 创建一个纹理图片加载器加载图片
       var textureLoader = new THREE.TextureLoader()
       // 加载背景图片
       var texture = textureLoader.load('/model/WechatIMG80.png')
       // 纹理对象Texture赋值给场景对象的背景属性.background
-      this.scene.background = texture
+      scene.background = texture
     },
     init2 () {
       const container = document.querySelector('#container')
       this.scene = new THREE.Scene()
       // this.setBackground()
       this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 60, 100000)
-      new GLTFLoader().load('/model/test3.gltf', (gltf) => {
+      new GLTFLoader().load('/model/7211.gltf', (gltf) => {
         this.cameras = gltf.cameras
         this.cameras.push({
           position: {
@@ -146,7 +305,7 @@ export default {
       this.renderer.setClearAlpha(0.2)
       this.renderer.setSize(window.innerWidth, window.innerHeight)
       this.renderer.setClearColor(0xFFFFFF)
-      this.renderer.setPixelRatio(window.devicePixelRatio)
+      // this.renderer.setPixelRatio(window.devicePixelRatio)
       // this.renderer.gammaOutput = true
       // this.renderer.gammaFactor = 2.2
 
@@ -156,9 +315,9 @@ export default {
       // pmremGenerator.fromScene(new RoomEnvironment(), 0.04).texture
       // this.scene.environment = pmremGenerator
       container.appendChild(this.renderer.domElement)
-      const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5)
+      // const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1)
 
-      this.scene.add(ambientLight) // 环境光
+      // this.scene.add(ambientLight) // 环境光
 
       // const light1 = new THREE.HemisphereLight(0xffffbb, 0x080820, 1)
       // light1.position.set(-7, -7, 6.3)
@@ -167,15 +326,15 @@ export default {
       // const light2 = new THREE.HemisphereLight(0xffffbb, 0x080820, 1)
       // this.scene.add(light2)
       const light = new THREE.DirectionalLight(0xffffff, 1) // 从正上方（不是位置）照射过来的平行光，0.45的强度
-      // light.position.set(0, 100, 0)
+      light.position.set(-1, 2, 4)
       // light.target.position.set(0, 0, 0)
 
-      const light2 = new THREE.HemisphereLight(0xffffff, 1) // 从正上方（不是位置）照射过来的平行光，0.45的强度
+      // const light2 = new THREE.HemisphereLight(0xffffff, 1) // 从正上方（不是位置）照射过来的平行光，0.45的强度
       // light2.position.set(0, 100, 0)
       // light2.target.position.set(0, 0, 0)
       this.setHelp(light)
       this.scene.add(light)
-      this.scene.add(light2)
+      // this.scene.add(light2)
       // this.scene.add(this.light.target)
 
       // const lightPoint = new THREE.PointLight(0xE9E9E9, 0.9)
@@ -193,24 +352,22 @@ export default {
       } = this
       this.camera.position.set(x, y, z)
 
-      this.$refs.btn.addEventListener('click', e => {
-        console.log('this.camera: ', this.camera)
-        const {
-          x,
-          y,
-          z
-        } = this
-        // this.camera.fov = 10
-        this.camera.position.x = x
-        this.camera.position.y = y
-        this.camera.position.z = z
-        console.log('this.camera.fov: ', this.camera.fov)
-        this.camera.updateProjectionMatrix()
-      })
+      // this.$refs.btn.addEventListener('click', e => {
+      //   console.log('this.camera: ', this.camera)
+      //   const {
+      //     x,
+      //     y,
+      //     z
+      //   } = this
+      //   // this.camera.fov = 10
+      //   this.camera.position.x = x
+      //   this.camera.position.y = y
+      //   this.camera.position.z = z
+      //   console.log('this.camera.fov: ', this.camera.fov)
+      //   this.camera.updateProjectionMatrix()
+      // })
 
       const animate = () => {
-        // cube.rotation.x += 0.01
-        // cube.rotation.y += 0.02
         this.controls.update()
         requestAnimationFrame(animate)
         this.renderer.render(this.scene, this.camera)
